3b7ba4316f9653855818dd8e2d0831140e871da4
Ifeanyi <chimaifeanyi29@gmail.com>
2021-12-10 18:00:05 +0100

    edit url

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ app.set("views", path.join(__dirname, "views"));
 
 
 //routes middlewrae
-app.use("/api/route|/|https://ifeanyi-nodejs-login-app.herokuapp.com", routes)
+app.use("/https://ifeanyi-nodejs-login-app.herokuapp.com/", routes)
+// app.use("/api/route|/|https://ifeanyi-nodejs-login-app.herokuapp.com", routes)
 // app.use("https://lace-sand-citipati.glitch.me|/", routes)
 
 
